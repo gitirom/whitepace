cddb92cea388af0a62e15423e4e46fd1f2c78ab7
gitirom <wadiaromdane10@gmail.com>
2025-01-04 22:25:06 +0100

    complete login page UI and some js functions

--- index.js
+++ index.js
@@ -10,3 +10,44 @@ cards.forEach(card => {
     });
 });
 
+
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!emailPattern.test(email)) {
+        alert('Please enter a valid email address');
+        return;
+    }
+
+    if (password.trim() === '' ||  password.length < 8) {
+        alert('Please enter your password');
+        return;
+    }
+
+    const toastElement = document.getElementById('successToast');
+    const toast = new bootstrap.Toast(toastElement);
+    toast.show();
+
+    
+    setTimeout(() => {
+        window.location.href = 'home.html'; 
+    }, 1000);
+});
+
+
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password');
+    const toggleIcon = document.getElementById('toggleIcon');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        toggleIcon.src = '/images/hidden.svg'; 
+    } else {
+        passwordInput.type = 'password';
+        toggleIcon.src = '/images/eye.svg'; 
+    }
+}

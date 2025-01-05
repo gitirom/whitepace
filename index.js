a304f085e59e27ae005967c57abc84d14852ffd3
gitirom <wadiaromdane10@gmail.com>
2025-01-05 22:11:59 +0100

    complete register page UI

--- index.js
+++ index.js
@@ -38,6 +38,43 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     }, 1000);
 });
 
+// Register form validation
+document.getElementById('register-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    if (name.trim() === '') {
+        alert('Please enter your name');
+        return;
+    }
+
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!emailPattern.test(email)) {
+        alert('Please enter a valid email address');
+        return;
+    }
+
+    if (password.trim() === '' || password.length < 8) {
+        alert('Password must be at least 8 characters long');
+        return;
+    }
+
+    console.log(name, email, password);
+    
+
+    const toastElement = document.getElementById('successToast');
+    const toast = new bootstrap.Toast(toastElement);
+    toast.show();
+
+    setTimeout(() => {
+        window.location.href = 'login.html';
+    }, 1000);
+});
+
+
 
 function togglePasswordVisibility() {
     const passwordInput = document.getElementById('password');
